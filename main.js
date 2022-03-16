f93a27882a8b6ed17f60335c4ed20df479d94275
Ron Suhodrev <ron.suhodrev@gmail.com>
2022-03-16 10:00:35 +0800

    Made the bottom bar collapsible

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ content.innerHTML = contentInput.value
 contentInput.addEventListener("input", function(e) {
   content.innerHTML = e.target.value;
 });
+
+const root = document.querySelector(':root')
+root.style.setProperty('--bottom-bar-height', '90px')
+
+const collapseBtn = document.querySelector('#collapse-btn')
+const bottomBar = document.querySelector('#bottom-bar')
+collapseBtn.addEventListener("click", () => {
+  root.style.setProperty('--bottom-bar-height', root.style.getPropertyValue('--bottom-bar-height') === '90px' ? '20px' : '90px')
+})
